8c357f32de25f1ca5e7a33ebd258b8d5c69917cb
Abe Handler <akh2103@gmail.com>
2018-10-26 13:16:26 -0400

    just backup

--- preproc/split_validation_and_test.py
+++ preproc/split_validation_and_test.py
@@ -12,6 +12,7 @@ random.seed(1)
 
 validation_size = 25000
 
+
 def load_dataset():
     sources = []
     for source in glob.glob("sentence-compression/data/*sent-comp*source"):
@@ -40,9 +41,51 @@ if __name__ == "__main__":
         dt = [json.dumps(_) for _ in data[0:-validation_size]]
         print len(dt)
         of.write("\n".join(dt))
- 
+
     print "[*] dumped validation examples"
     # this is to train the ILP from F & A
     with open("preproc/100k", "w") as of:
         dt = [filippova_tree_transform(json.loads(i)) for i in dt[0:100000]] 
-        pickle.dump(dt,of) 
+        pickle.dump(dt, of)
+
+    print "[*] building vocab"
+
+    def get_toks(jdoc):
+        for t in jdoc['tokens']:
+            yield t
+
+    def get_deps(jdoc):
+        for t in jdoc['enhancedDependencies']:
+            yield t["dep"]
+
+    V = set()
+    dep_v = set(["ROOT"])
+    pos_v = set(["ROOT"])
+    ner_v = set(["O"])
+    lemma_v = set(["ROOT"])
+    lemma_v_dep_v = set(["ROOT-ROOT"])
+    for ln2 in dt:
+        for t in get_toks(ln2):
+            V.add(t["word"])
+            pos_v.add(unidecode(t["pos"]))
+            assert type(t["pos"]) != str
+            ner_v.add(unidecode(t['ner']))
+            assert type(t["ner"]) != str
+            lemma_v.add(unidecode(t['lemma']))
+            assert type(t["lemma"]) != str
+        for d in ln2["enhancedDependencies"]:
+            try:
+                lemma = get_tok(d["governor"], jdoc=ln2)["lemma"]
+                lemma_v_dep_v.add(unidecode(lemma + "-" + d["dep"]))
+            except:
+                logger.warning("this dep points to non existing token$${}$${}".format(ln, json.dumps(d)))
+        for d in get_deps(ln2):
+            dep_v.add(unidecode(d))
+
+    with open(sys.argv[1] + ".vocabs", "w") as of:
+        of.write(json.dumps({"V": list(V),
+                             "pos_v": list(pos_v),
+                             "dep_v": list(dep_v),
+                             "ner_v": list(ner_v),
+                             "lemma_v_dep_v": list(lemma_v_dep_v),
+                             "lemma_v": list(lemma_v)}))
