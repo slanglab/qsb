b6f59e1c0eb8e1d0f61a9b497b5bdb3c02787095
Abe Handler <akh2103@gmail.com>
2018-10-28 13:40:22 -0400

    just backup

--- nn/go.sh
+++ nn/go.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source activate allennlp
 export PYTHONPATH=.
-t="experiments/"$(date +%s)
-allennlp train $1 -s $t --include-package lib
-allennlp predict $t/model.tar.gz tests/fixtures/s2_papers.jsonl --include-package lib --predictor paper-classifier --output-file $t/preds.jsonl
+t="experiments/compressor/"$(date +%s)
+allennlp train $1 -s $t --include-package nn 
+allennlp predict $t/model.tar.gz tests/fixtures/s2_papers.jsonl --include-package nn --predictor paper-classifier --output-file $t/preds.jsonl
