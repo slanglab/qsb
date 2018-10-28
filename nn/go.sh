554ec7a69fa26375c567ae69b114b56370e8d5b6
Abram Handler <ahandler@gypsum.cm.cluster>
2018-10-28 14:51:53 -0400

    nn go

--- nn/go.sh
+++ nn/go.sh
@@ -3,4 +3,4 @@ source activate allennlp
 export PYTHONPATH=.
 t="/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/"$(date +%s)
 allennlp train $1 -s $t --include-package nn 
-allennlp predict $t/model.tar.gz tests/fixtures/s2_papers.jsonl --include-package nn --predictor paper-classifier --output-file $t/preds.jsonl
+allennlp predict $t/model.tar.gz tests/fixtures/sample.jsonl --include-package nn --predictor paper-classifier --output-file $t/preds.jsonl
