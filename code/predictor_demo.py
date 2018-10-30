85448294db2f3ba293a44bdbd2b872ce079c3ae5
Abram Handler <ahandler@gypsum.cm.cluster>
2018-10-30 13:41:00 -0400

    stuff

---
+++ code/predictor_demo.py
@@ -0,0 +1,29 @@
+from allennlp.predictors.predictor import Predictor
+from allennlp.models.archival import load_archive
+import nn.models
+import json
+archive_file = "/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/633320059/model.tar.gz"
+archive = load_archive(archive_file=archive_file)
+predictor = Predictor.from_archive(archive, "paper-classifier") 
+
+sentence = "Hello world"
+label = "NA"
+
+fn = "tests/fixtures/sample.jsonl"
+
+with open(fn, "r") as inf:
+    for _ in inf:
+        _ = json.loads(_)
+        sentence = " ".join(_["tokens"])
+        label = _["label"]
+        instance = predictor._dataset_reader.text_to_instance(sentence, label)
+
+        # allennlp predict /mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/633320059/model.tar.gz tests/fixtures/sample.jsonl --include-package nn --predictor paper-classifier --output-file /mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/633320059/preds.jsonl
+
+        print(predictor.predict_instance(instance))
+
+
+with open("/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/633320059/preds.jsonl", "r") as inf:
+    for _ in inf:
+        _ = json.loads(_)
+        print(_)
