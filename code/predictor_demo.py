cf923822bd3ed75a69dfec42d7df0a17cead829a
Abram Handler <ahandler@gypsum.cm.cluster>
2018-10-30 14:44:15 -0400

    ok

--- code/predictor_demo.py
+++ code/predictor_demo.py
@@ -2,7 +2,7 @@ from allennlp.predictors.predictor import Predictor
 from allennlp.models.archival import load_archive
 import nn.models
 import json
-archive_file = "/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/633320059/model.tar.gz"
+archive_file = "tests/fixtures/633320059/model.tar.gz"
 archive = load_archive(archive_file=archive_file)
 predictor = Predictor.from_archive(archive, "paper-classifier") 
 
