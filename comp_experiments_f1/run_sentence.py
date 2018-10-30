98385cc9ffad5aab7e45d5109a1862004675c25c
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-10-30 15:30:09 -0400

    f1

--- comp_experiments_f1/run_sentence.py
+++ comp_experiments_f1/run_sentence.py
@@ -6,7 +6,7 @@ from sklearn.metrics import f1_score
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("sentence", type=int)
+    parser.add_argument("-sentence", type=int)
     args = parser.parse_args()
 
     with open("preproc/validation.jsonl", "r") as inf:
