40804273b347a20a304173a1c4d6a67ce9b4497c
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-10-30 19:26:02 -0400

    stuf

--- comp_experiments_f1/run_sentence.py
+++ comp_experiments_f1/run_sentence.py
@@ -15,19 +15,18 @@ if __name__ == "__main__":
 
     assert args.sentence is not None
 
+    range_ = range(args.sentence * 100, args.sentence * 100 + 100)
+
     with open("preproc/validation.jsonl", "r") as inf:
         for vno, _ in enumerate(inf):
-            if vno == args.sentence:
+            if vno in range_:
                 sentence = json.loads(_)
-                break
-
-    if True: # args.model
-        model = NeuralNetworkTransitionGreedy(args.archive_loc)
-        orig_ix = [_["index"] for _ in sentence["tokens"]]
-        y_true = [_ in sentence["compression_indexes"] for _ in orig_ix]
-        y_pred = model.predict(sentence)
-        f1 = f1_score(y_true=y_true, y_pred=y_pred)
-        out_ = args.results_dir + "/{}-{}".format(args.sentence,
-                                                  args.model)
-        with open(out_, "w") as of:
-            of.write(str(f1))
+                model = NeuralNetworkTransitionGreedy(args.archive_loc)
+                orig_ix = [_["index"] for _ in sentence["tokens"]]
+                y_true = [_ in sentence["compression_indexes"] for _ in orig_ix]
+                y_pred = model.predict(sentence)
+                f1 = f1_score(y_true=y_true, y_pred=y_pred)
+                out_ = args.results_dir + "/{}-{}".format(vno,
+                                                          args.model)
+                with open(out_, "w") as of:
+                    of.write(str(f1))
