aff9c48476e90368e3e844cf7df2899810119986
Abe Handler <akh2103@gmail.com>
2018-11-02 09:53:05 -0400

    just backup

--- comp_experiments_f1/run_sentence.py
+++ comp_experiments_f1/run_sentence.py
@@ -1,40 +1,49 @@
 import json
-import csv
 import argparse
 
 from sklearn.metrics import f1_score
 from comp_experiments_f1.algorithms import NeuralNetworkTransitionGreedy
 
+
 def strip_tags(tokens):
-    return [_ for _ in tokens if "SOS" not in _["word"] and "EOS" not in _["word"] and "OOV" not in _["word"]]
+    return [_ for _ in tokens if "SOS" not in _["word"] and "EOS"
+            not in _["word"] and "OOV" not in _["word"]]
+
+
+def get_model(model):
+    if model == "nn-greedy-query":
+        return NeuralNetworkTransitionGreedy(config.archive_loc)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-sentence", type=int)
-    parser.add_argument("-model", type=str, default="nn")
-    parser.add_argument("-archive_loc", type=str, default="tests/fixtures/891477193/model.tar.gz")
-    parser.add_argument("-results_dir", type=str, default="comp_experiments_f1/output/")
+    parser.add_argument("-start", type=int)
+    parser.add_argument("-config", type=int)
     args = parser.parse_args()
 
-    assert args.sentence is not None
+    with open(args.config, "r") as inf:
+        config = json.load(inf)
 
-    range_ = range(args.sentence * 100, args.sentence * 100 + 100)
+    assert args.start is not None
 
-    print(range_)
+    range_ = range(args.start * 100, args.start * 100 + 100)
 
     with open("preproc/lstm_validation.jsonl", "r") as inf:
         for vno, _ in enumerate(inf):
             if vno in range_:
                 sentence = json.loads(_)
                 sentence["tokens"] = strip_tags(sentence["tokens"])
-                model = NeuralNetworkTransitionGreedy(args.archive_loc)
+                model = get_model(config["model"])
                 orig_ix = sentence["original_ix"]
-                y_true = [_ in sentence["compression_indexes"] for _ in orig_ix]
+                y_true = [_ in sentence["compression_indexes"] for
+                          _ in orig_ix]
                 out = model.predict(sentence)
                 y_pred = out["y_pred"]
-                ops = out["nops"]        
+                ops = out["nops"]
                 f1 = f1_score(y_true=y_true, y_pred=y_pred)
-                out_ = args.results_dir + "/{}-{}".format(vno,
-                                                          args.model)
-                with open(out_, "w") as of:
-                    of.write(str(f1))
+                out_ = config.results_dir + "/{}-{}".format(vno,
+                                                            config.model)
+                config[sentence] = out_
+
+    with open(out_, "w") as of:
+        json.dump(config, of)
