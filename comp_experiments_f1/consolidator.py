9d53bd2def588d7ac5eef827b1a429a6ef6bb58a
Abe Handler <akh2103@gmail.com>
2018-11-02 10:18:45 -0400

    just backup

--- comp_experiments_f1/consolidator.py
+++ comp_experiments_f1/consolidator.py
@@ -2,10 +2,16 @@ import glob
 import numpy as np
 
 models = ["nn"]
-for mo in models:
-    all_ = []
-    for fn in glob.glob("comp_experiments_f1/output/*-nn"):
-        with open(fn, "r") as inf:
-            dt = inf.read()
-            all_.append(float(dt))
-    print np.mean(all_) 
+
+for fn in glob.glob("comp_experiments_f1/output/*"):
+    with open(fn, "r") as inf:
+        dt = inf.read()
+        f1s = []
+        nops = []
+        for k, v in dt.items():
+            if "sentence" in k:
+                f1s.append(float(v["f1"]))
+                nops.append(float(v["nops"]))
+        f1s = np.mean(f1s)
+        ops = np.mean(nops)
+        print f1s, nops
