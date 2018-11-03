74837eb3471a44b69fc4fcd4022e1a8cf9ecd32f
Abe Handler <akh2103@gmail.com>
2018-11-03 07:24:27 -0400

    just backup

--- preproc/lstm_preproc.py
+++ preproc/lstm_preproc.py
@@ -11,6 +11,9 @@ from collections import Counter
 from code.utils import get_labeled_toks
 from code.printers import pretty_print_conl
 
+PP = "proposedprune"
+PE = 'proposedextract'
+
 
 def get_root(w):
     return [_["dependent"] for _ in w["basicDependencies"]
@@ -26,9 +29,63 @@ def is_prune_only(jdoc):
     return gov == 0 and one_extract
 
 
+def get_r(jdoc):
+    return len(" ".join([i["word"] for i in jdoc["tokens"] if i["index"]
+               in jdoc["compression_indexes"]]))
+
+
+def get_oracle_label(jdoc, node, state):
+    oracle_label = jdoc["oracle"][str(node)]
+    vc = [i["index"] for i in state["tokens"]]
+    if oracle_label == "NA":
+        if node in vc:
+            oracle_label = "np"  # no prune
+        else:
+            oracle_label = "ne"  # no extract
+    return vc
+
+
+def get_governing_dep(original_s, node):
+    return [ii["dep"] for ii in original_s["basicDependencies"]
+            if ii["dependent"] == node][0]
+
+
+def get_instance(original_s, node, state):
+    orig_ix = [i["index"] for i in _["tokens"]]
+    oracle_label = get_oracle_label(original_s, node, state)
+    encoding = {
+                "compression_indexes": _["compression_indexes"],
+                "label": oracle_label,
+                "dep": get_governing_dep(original_s, node),
+                "q": _['q'],
+                "r": get_r(original_s),
+                "original_ix": orig_ix,
+                "basicDependencies": original_s["basicDependencies"]
+                }
+    return encoding
+
+
+def get_proposed(original_s, node, state):
+    proposed = extract_for_state(g=original_s, v=node)
+    proposed["tokens"] = state["tokens"] + proposed["tokens"]
+    proposed["basicDependencies"] = state["basicDependencies"] + proposed["basicDependencies"]
+    return proposed
+
+
+def get_encoded_tokens(instance, state, original_s, node):
+    if instance["oracle_label"] in ["p", "np"]:
+        encoded_tokens = get_labeled_toks(node, state, PP)
+    elif instance["oracle_label"] in ["e", "ne"]:
+        proposed = get_proposed(original_s, node, state)
+        encoded_tokens = get_labeled_toks(node, proposed, PE)
+    else:
+        assert "bad" == "thing"
+    return encoded_tokens
+
+
 def save_split_3way(fn, data, cap=None):
     '''
-    note, avoiding pulling a whole big corpus into memory so this can scale up
+    note, avoiding pulling a whole big corpus into memory
     inputs:
         fn (str): output file
         data (list<int>): a list of lines that are included in training set
@@ -42,55 +99,29 @@ def save_split_3way(fn, data, cap=None):
                     break  # early stopping
                 if ino in data:
                     _ = json.loads(_)
-                    se_ve = copy.deepcopy(_)
-                    orig_ix = [i["index"] for i in _["tokens"]]
-                    r = len(" ".join([i["word"] for i in _["tokens"] if i["index"]
-                                     in _["compression_indexes"]]))
+                    original_s = copy.deepcopy(_)
                     walk = get_walk_from_root(_)
                     state = {"tokens": [], "basicDependencies": []}
                     for node in walk:
-                        oracle_label = _["oracle"][str(node)]
-                        if oracle_label == "NA":
-                            if node in walk:
-                                oracle_label = "np"  # no prune
-                            else:
-                                oracle_label = "ne"  # no extract
+                        instance = get_instance(original_s, node, state)
+                        instance["tokens"] = get_encoded_tokens(instance,
+                                                                state,
+                                                                original_s,
+                                                                node)
+                        of.write(json.dumps(instance) + "\n")
 
-                        dep = [ii["dep"] for ii in _["basicDependencies"]
-                               if ii["dependent"] == node][0]
-
-                        encoding = {
-                            "compression_indexes": _["compression_indexes"],
-                            "label": oracle_label,
-                            "dep": dep,
-                            "q": _['q'],
-                            "r": r,
-                            "original_ix": orig_ix,
-                            "basicDependencies": se_ve["basicDependencies"]
-                        }
-
-                        if oracle_label in ["p", "np"]:
-                            encoded_tokens = get_labeled_toks(node, state, "p")
-                        elif oracle_label in ["e", "ne"]:
-                            proposed = extract_for_state(g=se_ve, v=node)
-                            proposed["tokens"] = state["tokens"] + proposed["tokens"]
-                            proposed["basicDependencies"] = state["basicDependencies"] + proposed["basicDependencies"]
-                            encoded_tokens = get_labeled_toks(node, proposed, "e")
-                        else:
-                            assert "bad" == "thing"
-
-                        encoded_tokens.sort(key=lambda x: float(x["index"]))
-                        encoding["tokens"] = encoded_tokens
-
-                        of.write(json.dumps(encoding) + "\n")
-                        total_so_far += 1
-                        if encoding["label"] == "p":
+                        # update state if relevant
+                        if instance["label"] == "p":
                             prune(g=state, v=node)
-                        if encoding["label"] == "e":
+                        if instance["label"] == "e":
+                            proposed = get_proposed(original_s, node, state)
                             state["tokens"] = proposed["tokens"]
                             state["basicDependencies"] = proposed["basicDependencies"]
+                        total_so_far += 1
+
                 transition = [o["index"] for o in state["tokens"]]
                 assert set(transition) == set(_["compression_indexes"])
+
 
 if __name__ == "__main__":
 
