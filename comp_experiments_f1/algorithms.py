1d2a7c208d0fdf66b4ee7ae7f9b389198f47ba43
Abe Handler <akh2103@gmail.com>
2018-11-03 10:08:56 -0400

    just backup

--- comp_experiments_f1/algorithms.py
+++ comp_experiments_f1/algorithms.py
@@ -2,17 +2,18 @@ from allennlp.predictors.predictor import Predictor
 from allennlp.models.archival import load_archive
 from code.utils import get_labeled_toks
 from code.treeops import prune
-from code.treeops import bfs
 from code.utils import prune_deletes_q
 from code.treeops import get_walk_from_root
+from preproc.lstm_preproc import get_instance
+from preproc.lstm_preproc import get_encoded_tokens
 import nn.models
-import json
 
 
 class NeuralNetworkTransitionGreedy:
     def __init__(self, archive_loc, query_focused=True):
         assert type(archive_loc) == str
         archive = load_archive(archive_file=archive_loc)
+        self.archive = archive
         self.query_focused = query_focused
         self.predictor = Predictor.from_archive(archive, "paper-classifier")
 
@@ -22,7 +23,8 @@ class NeuralNetworkTransitionGreedy:
         according to transition-based nn model
         '''
         label = "NA"
-        sentence = " ".join([_["word"] for _ in get_labeled_toks(vertex, jdoc)])
+        sentence = get_labeled_toks(vertex, jdoc)
+        assert "old" == "way"
         instance = self.predictor._dataset_reader.text_to_instance(sentence,
                                                                    label)
         pred = self.predictor.predict_instance(instance)
@@ -73,7 +75,7 @@ class NeuralNetworkTransitionGreedy:
                     }
 
 
-class NeuralNetworkTransitionBFS:
+class NeuralNetworkTransitionBFSPruneOnly:
 
     def __init__(self, archive_loc):
         assert type(archive_loc) == str
@@ -114,3 +116,50 @@ class NeuralNetworkTransitionBFS:
             T -= .1
         remaining_toks = [_["index"] for _ in jdoc["tokens"]]
         return [_ in remaining_toks for _ in orig_toks]
+
+
+class NeuralNetworkTransitionBFS:
+
+    def __init__(self, archive_loc, query_focused=True):
+        assert type(archive_loc) == str
+        archive = load_archive(archive_file=archive_loc)
+        self.query_focused = query_focused
+        self.predictor = Predictor.from_archive(archive, "paper-classifier")
+
+    def predict_proba(self, jdoc, vertex):
+        '''
+        what is probability that this vertex is prunable,
+        according to transition-based nn model
+        '''
+        label = "NA"
+        sentence = " ".join([_["word"] for _ in get_labeled_toks(vertex, jdoc)])
+        instance = self.predictor._dataset_reader.text_to_instance(sentence,
+                                                                   label)
+        pred = self.predictor.predict_instance(instance)
+        return pred["class_probabilities"]
+
+    def get_char_length(self, jdoc):
+        assert type(jdoc["tokens"][0]["word"]) == str
+        return len(" ".join([_["word"] for _ in jdoc["tokens"]]))
+
+    def predict(self, original_s):
+        '''
+        return a compression that preserves q and respects r
+        '''
+
+        state = {"tokens": [], "basicDependencies": []}
+
+        def in_compression(vertex, state_):
+            return vertex in [i["index"] for i in state_["tokens"]]
+
+        for vertex in get_walk_from_root(original_s):
+            instance = get_instance(original_s, vertex, state)
+            if in_compression(vertex, state):
+                proposed = "p"
+            else:
+                proposed = "e"
+            instance = get_encoded_tokens(instance, state, original_s,
+                                          vertex, proposed)
+            instance = self.predictor._dataset_reader.text_to_instance(instance,
+                                                                       "NA")
+            pred = self.predictor.predict_instance(instance)
