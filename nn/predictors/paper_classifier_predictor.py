8a5312e5bf70196a686ff48ba4cb0954512615cb
Abram Handler <ahandler@gypsum.cm.cluster>
2018-11-03 07:45:34 -0400

    ok

--- nn/predictors/paper_classifier_predictor.py
+++ nn/predictors/paper_classifier_predictor.py
@@ -11,6 +11,6 @@ class PaperClassifierPredictor(Predictor):
     """"Predictor wrapper for the AcademicPaperClassifier"""
     @overrides
     def _json_to_instance(self, json_dict: JsonDict) -> Instance:
-        sentence = " ".join(json_dict['tokens'])
+        sentence = " ".join([i["word"] for i in json_dict['tokens']])
         instance = self._dataset_reader.text_to_instance(sentence=sentence)
         return instance
