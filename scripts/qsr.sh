2f0cc0b62494dfe2885a150578e00727d62d6888
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-11-14 18:47:07 -0500

    qs

--- scripts/qsr.sh
+++ scripts/qsr.sh
@@ -2,7 +2,8 @@
 source activate allennlp
 
 rm comp_experiments_f1/output/*
-python comp_experiments_f1/run_sentence.py -fast -config comp_experiments_f1/experiments/prune_only_nn.json
-python comp_experiments_f1/run_sentence.py -fast -config comp_experiments_f1/experiments/ilp.json
+python comp_experiments_f1/run_sentence.py -config comp_experiments_f1/experiments/prune_only_nn.json
+python comp_experiments_f1/run_sentence.py -config comp_experiments_f1/experiments/ilp.json
+python comp_experiments_f1/run_sentence.py -config comp_experiments_f1/experiments/nn_pred_then_prune.json
+
 python comp_experiments_f1/consolidator.py
-py comp_experiments_f1/run_sentence.py -fast -config comp_experiments_f1/experiments/nn_pred_then_prune.json -verbose
