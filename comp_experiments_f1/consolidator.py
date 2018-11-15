6fb8ad299c389d9f8c65bb7ec9bd699ae6070be9
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-11-15 14:16:38 -0500

    okok

--- comp_experiments_f1/consolidator.py
+++ comp_experiments_f1/consolidator.py
@@ -9,12 +9,14 @@ results = defaultdict(lambda: defaultdict(list))
 for fn in glob.glob("comp_experiments_f1/output/*"):
     with open(fn, "r") as inf:
         dt = json.load(inf)
+        print dt.items()
         f1s = []
         nops = []
         for k, v in dt.items():
             if "sentence" in k:
                 results[dt["algorithm"]]["f1"].append(v["f1"])
                 results[dt["algorithm"]]["nops"].append(v["nops"])
+                print v["f1"]
             results[dt['algorithm']]["no_compression"] = dt["no_compression"]
 
 for model in results:
