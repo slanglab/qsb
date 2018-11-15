0272aaa0723da43e8d20f109af20d8e6b8131795
Abram Handler <ahandler@gypsum.cm.cluster>
2018-11-15 13:37:36 -0500

    ok

--- tuning/scores_to_csv.py
+++ tuning/scores_to_csv.py
@@ -4,54 +4,40 @@ import glob
 scores = {}
 best_epochs = {}
 
-OUT = "/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/*/*metrics.json"
 
-config = "/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/*/*config.json"
-
-for fn in glob.glob(OUT):
-    run = fn.split("/")[-2]
-    print(fn)
-    with open(fn, "r") as inf:
-        dt = json.load(inf)
-        scores[run] = dt["best_validation_accuracy"]
-        best_epochs[run] = dt["best_epoch"]
+out = []
+experiments = "/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/*"
 
 configs = {}
 
-for _ in glob.glob(config):
-    #print(_)
-    with open(_, "r") as inf:
-        try:
+for experiment in glob.glob(experiments):
+    for metric_epoch in glob.glob(experiment + "/" + "*metrics_epoch*"):
+        stats = []
+        with open(metric_epoch, "r") as inf:
             dt = json.load(inf)
-            #print(_)
-            #print(configs.keys())
-            configs[_.split("/")[-2]] = dt
-        except:
-            pass
-
-
-out = []
-
-out.append(["dropout", "hidden_size", "dropout_ff", "weight_decay", "lr", "score"])
-
-for _ in scores:
-    stats = [_]
-    try:
-        cf = configs[_] 
+            epoch_metric = float(dt["validation_accuracy"])
+            epoch = dt["epoch"]
+            print(epoch_metric,epoch)
+        with open(experiment + "/config.json", "r") as inf2:
+            cf = json.load(inf2)
+        stats.append(experiment)
+        stats.append(epoch)
         stats.append(cf["model"]["abstract_encoder"]["dropout"])
         stats.append(cf["model"]["abstract_encoder"]["hidden_size"])
         stats.append(cf["model"]["classifier_feedforward_i"]["dropout"]) 
         stats.append(cf["trainer"]["optimizer"]["weight_decay"])
         stats.append(cf["trainer"]["optimizer"]["lr"])
-        stats.append(scores[_])
-        if not type(stats[2]) == list:
-            out.append(stats)
-            print("***")
-            print(_)
-            print(scores[_])
-    except KeyError:
-        print(_)
+        stats.append(cf['model']['text_field_embedder']['tokens']['embedding_dim'])
+        stats.append(epoch_metric)
+        out.append(stats)
+    
+out.sort(key=lambda x:x[-1], reverse=True)
+
+
 
 with open("tuning/tuner.csv", "w") as of:
     csv_writer = csv.writer(of)
+
+    csv_writer.writerow(["experiment","epoch","dropout", "hidden_size", "dropout_ff", "weight_decay", "lr", "embed_dim", "score"])
+
     csv_writer.writerows(out)
