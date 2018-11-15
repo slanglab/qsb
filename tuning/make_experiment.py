59d88b4cd78bf78e64fffb29f21b9127671573ba
Abe Handler <akh2103@gmail.com>
2018-11-15 07:21:21 -0500

    just backup

--- tuning/make_experiment.py
+++ tuning/make_experiment.py
@@ -15,25 +15,38 @@ experiment = sys.argv[1]
 for fn in glob.glob("/mnt/nfs/scratch1/ahandler/experiments/qsr/*json"):
     os.remove(fn)
 
+
 def make_rando():
+
+    inputd = [300, 100]
+    random.shuffle(inputd)
 
     with open(experiment, "r") as inf:
         dt = json.load(inf)
 
-    dt['model']["abstract_encoder"]["hidden_size"] = random.randint(100,1000)
+    fn = "https://s3-us-west-2.amazonaws.com/allennlp/datasets/glove/glove.6B.{}d.txt.gz".format(inputd)
+    dt['model']['text_field_embedder']['pretrained_file'] = fn
+    dt['model']['text_field_embedder']['embedding_dim'] = inputd
+    dt["model"]["abstract_encoder"]["hidden_size"] = inputd
+
+    dt['model']["abstract_encoder"]["hidden_size"] = random.randint(100, 1000)
     dt['model']["abstract_encoder"]['dropout'] = random.uniform(0, .1)
     dt["model"]['classifier_feedforward_i']["dropout"] = random.uniform(.2, .7)
     dt["model"]['classifier_feedforward_i']["input_dim"] = dt['model']["abstract_encoder"]["hidden_size"] * 2
 
-
     dt["model"]['classifier_feedforward_p']["dropout"] = dt["model"]['classifier_feedforward_i']["dropout"]
     dt["model"]['classifier_feedforward_p']["input_dim"] = dt['model']["classifier_feedforward_i"]["input_dim"]
 
-    x = random.uniform(2, 5)
+    layers = [1, 2, 3, 4, 5]
+    random.shuffle(layers)
+    num_layers = layers[0]
+    dt["abstract_encoder"]["num_layers"] = num_layers
 
-    dt['trainer']['optimizer']["lr"] = 10 ** -x * random.uniform(1,10)
+    x = random.uniform(2, 7)
 
-    x = random.uniform(3, 5)
+    dt['trainer']['optimizer']["lr"] = 10 ** -x * random.uniform(1, 10)
+
+    x = random.uniform(2, 7)
 
     dt['trainer']['optimizer']['weight_decay'] = 10 ** -x * random.uniform(1, 10)
 
