350fd269b31e671b48f24e396b86a1ec2ce26a02
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-11-15 15:26:00 -0500

    fk

--- tests/allen/two_embeddings_model_test.py
+++ tests/allen/two_embeddings_model_test.py
@@ -2,11 +2,12 @@
 from allennlp.common.testing import ModelTestCase
 
 
+
 class TwoEmbedsClassifier(ModelTestCase):
     def setUp(self):
         super(TwoEmbedsClassifier, self).setUp()
         self.set_up_model('tests/fixtures/two_embed.json',
-                          'tests/fixtures/s2_papers.jsonl')
+                          'tests/fixtures/validation_3way.jsonl')
 
     def test_model_can_train_save_and_load(self):
         self.ensure_model_can_train_save_and_load(self.param_file)
