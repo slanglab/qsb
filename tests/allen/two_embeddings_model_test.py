78ff3b1a98c0a304a97af8a9202174cc6e473b92
Abe Handler <akh2103@gmail.com>
2018-11-15 15:23:21 -0500

    just backup

--- tests/allen/two_embeddings_model_test.py
+++ tests/allen/two_embeddings_model_test.py
@@ -5,7 +5,7 @@ from allennlp.common.testing import ModelTestCase
 class TwoEmbedsClassifier(ModelTestCase):
     def setUp(self):
         super(TwoEmbedsClassifier, self).setUp()
-        self.set_up_model('tests/fixtures/academic_paper_classifier.json',
+        self.set_up_model('tests/fixtures/two_embed.json',
                           'tests/fixtures/s2_papers.jsonl')
 
     def test_model_can_train_save_and_load(self):
