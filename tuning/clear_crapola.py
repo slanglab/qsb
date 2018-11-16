227f9c9f4892bd0f6993d657e4ec96351484f811
Abram Handler <ahandler@gypsum.cm.cluster>
2018-11-16 11:51:36 -0500

    f

--- tuning/clear_crapola.py
+++ tuning/clear_crapola.py
@@ -3,7 +3,23 @@
 ### this clears out the bad to save disk
 
 import json
+import glob
+
+print(sum(1 for i in glob.glob("/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/*")))
+
+crapola = []
 
 for experiment in glob.glob("/mnt/nfs/scratch1/ahandler/experiments/qsr/compressor/*"):
-    with open(experiment+ "/metrics_epoch_1.json") as inf:
-        dt = json.load(inf)
+    try:
+        with open(experiment+ "/metrics_epoch_1.json") as inf:
+            dt = json.load(inf)
+            if dt["best_validation_accuracy"] < .80:
+                crapola.append(experiment)
+    except FileNotFoundError:
+        pass
+
+import shutil
+for c in crapola:
+    shutil.rmtree(c)
+
+print("removed {} failed experiments".format(len(crapola)))
