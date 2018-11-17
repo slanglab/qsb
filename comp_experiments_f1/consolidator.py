b650627648033f6578ec84e0142d776acabe82ad
Abe Handler <akh2103@gmail.com>
2018-11-17 08:16:01 -0500

    just backup

--- comp_experiments_f1/consolidator.py
+++ comp_experiments_f1/consolidator.py
@@ -9,15 +9,19 @@ results = defaultdict(lambda: defaultdict(list))
 for fn in glob.glob("comp_experiments_f1/output/*"):
     with open(fn, "r") as inf:
         dt = json.load(inf)
-        print dt.items()
-        f1s = []
-        nops = []
+        f1s_prunes = []
         for k, v in dt.items():
             if "sentence" in k:
                 results[dt["algorithm"]]["f1"].append(v["f1"])
                 results[dt["algorithm"]]["nops"].append(v["nops"])
-                print v["f1"]
+                results[dt["algorithm"]]["f1"].append(v["prunes"])
+                f1s_prunes.append((v["f1"], v["prunes"]))
             results[dt['algorithm']]["no_compression"] = dt["no_compression"]
+
+with open("/tmp/f1vprunes.txt", "w") as of:
+    for _ in f1s_prunes:
+        f1, p = _
+        of.write("{},{}".format(f1, p))
 
 for model in results:
     print(model)
