ea92416420bf2f0de8351fc05bb8e4e190f2e40e
Abe Handler <akh2103@gmail.com>
2018-11-21 17:16:44 -0500

    just backup

---
+++ tuning/perturb_good_one.py
@@ -0,0 +1,44 @@
+from __future__ import division
+import json
+import random
+import sys
+import os
+import glob
+
+experiment = sys.argv[1]
+
+for fn in glob.glob("/mnt/nfs/scratch1/ahandler/experiments/qsr/*json"):
+    os.remove(fn)
+
+
+def make_rando():
+
+    with open(experiment, "r") as inf:
+        dt = json.load(inf)
+
+    if random.uniform(0, 1) < .5:
+        dt['trainer']['optimizer']["lr"] += dt['trainer']['optimizer']["lr"] * random.uniform(.01,.05)
+    else:
+        dt['trainer']['optimizer']["lr"] -= dt['trainer']['optimizer']["lr"] * random.uniform(.01,.05)
+
+    if random.uniform(0, 1) < .5:
+        dt['trainer']['optimizer']['weight_decay'] += dt['trainer']['optimizer']['weight_decay'] * random.uniform(.01,.05)
+    else:
+        dt['trainer']['optimizer']['weight_decay'] -= dt['trainer']['optimizer']['weight_decay'] * random.uniform(.01,.05)
+
+    if random.uniform(0, 1) < .5:
+        dt['iterator']['batch_size'] += int(dt['iterator']['batch_size'] * random.uniform(.01,.05))
+    else:
+        dt['iterator']['batch_size'] -= int(dt['iterator']['batch_size'] * random.uniform(.01,.05))
+
+    import uuid
+
+    uuid_ex = str(uuid.uuid1())[0:8]
+
+    with open("/mnt/nfs/scratch1/ahandler/experiments/qsr/{}.json".format(uuid_ex), "w") as of:
+        json.dump(dt, of)
+
+    print(dt)
+
+for i in range(10):
+    make_rando()
