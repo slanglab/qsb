c0124aaf2a85367e7005590c2f77f2e15cc16f42
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-11-25 14:04:22 -0500

    g

--- ilp2013/fillipova_altun_supporting_code.py
+++ ilp2013/fillipova_altun_supporting_code.py
@@ -449,8 +449,6 @@ def syntactic(e, jdoc, vocabs):
     row_size = len(vocabs["pos_v"]) * 2 + len(vocabs['dep_v'])
 
     row = np.array([0, 0, 0])
-    print(pos(n,jdoc))
-    print(vocabs["pos_v2n"])
     col = np.array([vocabs['pos_v2n'][pos(h, jdoc)],
                     len(vocabs['pos_v2n']) + vocabs['pos_v2n'][pos(n, jdoc)],
                     len(vocabs['pos_v2n']) * 2 + vocabs["dep_v2n"][label(h, n, jdoc)]
