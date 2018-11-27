6b0950b02d147c6ef3e33a880ea53a63c24134f9
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-11-27 11:25:11 -0500

    asdf

---
+++ comp_experiments_part_1/trapezoid.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+# example useage $python comp_experiments_part_1/trapezoid.py comp_experiments_f1/output/fast-813281894-nn-prune-greedy 
+
+import json
+
+import sys
+
+fn = sys.argv[1]
+
+
+with open("comp_experiments_f1/output/fast-813281894-nn-prune-greedy", "r") as inf:
+    dt = json.load(inf)
+    dt = [v["nops"] for k,v in dt.items() if "sentence" in k]
+
+out = fn.split("/")[-1] + "trapezoid.csv"
+
+with open("output/" + out, "w") as of:
+    for o in dt:
+        of.write(",".join([str(i) for i in o]) + "\n") 
+
+#print(dt)
