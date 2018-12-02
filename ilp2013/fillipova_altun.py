5a3e694b0166dae6821c22bc7c41a2fb43ea0768
Abe Handler <akh2103@gmail.com>
2018-12-02 14:55:09 -0500

    bug. gurobi

--- ilp2013/fillipova_altun.py
+++ ilp2013/fillipova_altun.py
@@ -167,17 +167,11 @@ def run_model(jdoc, weights, vocab, k=1, r=100000, Q=[], verbose=False, force_de
         '''
         def get_Xs():
             for dep in jdoc["enhancedDependencies"]:
-                if Xs['{}-{}'.format(dep["governor"], dep["dependent"])].Xn > 0:
-                    yield {"word":dep["dependentGloss"], "dependent":dep["dependent"], "X":'{}-{}'.format(dep["governor"], dep["dependent"]),"X_val":Xs['{}-{}'.format(dep["governor"], dep["dependent"])].X}
-
-        def get_Xs_fix():
-            for dep in jdoc["enhancedDependencies"]:
                 if Xs['{}-{}'.format(dep["governor"], dep["dependent"])].Xn == 1:
                     yield {"word":dep["dependentGloss"], "dependent":dep["dependent"], "X":'{}-{}'.format(dep["governor"], dep["dependent"]),"X_val":Xs['{}-{}'.format(dep["governor"], dep["dependent"])].X}
 
-
         def get_compressed():
-            indexes = list(set([int(_.replace("w","")) for _ in Ws if Ws[_].Xn > 0]))
+            indexes = list(set([int(_.replace("w","")) for _ in Ws if Ws[_].Xn == 1]))
             indexes.sort()
             compressed = " ".join([_["word"] for _ in words if _["index"] in indexes])
             return compressed
@@ -190,7 +184,7 @@ def run_model(jdoc, weights, vocab, k=1, r=100000, Q=[], verbose=False, force_de
             def to_ints(key__):
                 a,b = key__.split("-")
                 return (int(a), int(b))
-            predicted = [to_ints(key_) for key_, v in Xs.items() if v.Xn > 0]
+            predicted = [to_ints(key_) for key_, v in Xs.items() if v.Xn == 1]
             predicted.sort(key=lambda x:x[0])
             compressed = get_compressed()
             out.append({"SolutionNumber":solution,
