6fe6bdf8e9f218635c52a7e5da9a9f779f44bf8b
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-12-06 15:58:51 -0500

    ok

--- comp_experiments_f1/consolidator.py
+++ comp_experiments_f1/consolidator.py
@@ -7,19 +7,26 @@ from collections import defaultdict
 results = defaultdict(lambda: defaultdict(list))
 
 
+def get_sentences(dt):
+    return [k for k in dt.keys() if "sentence" in k]
+
+def has_lm(dt):
+    return "lm" in dt[get_sentences(dt)[0]]
+
 for fn in glob.glob("comp_experiments_f1/output/*"):
     if "worst" not in fn:
         with open(fn, "r") as inf:
             dt = json.load(inf)
-            arch = dt["archive_loc"].replace("model.tar.gz", "metrics.json")
-            with open(arch, "r") as inf:
-                metrics =json.load(inf)
-            metrics["f1"] = np.mean([float(dt[v]["f1"]) for v in dt.keys() if "sentence" in v])
-            metrics["archive"] = dt["archive_loc"]
-            metrics['algo'] = dt['algorithm']
-            metrics['lm'] = np.mean([float(dt[v]["lm"]) for v in dt.keys() if "sentence" in v])
-            results[dt["algorithm"] + dt["archive_loc"]] = metrics
-            print(fn, metrics["f1"])
+            if has_lm(dt): 
+                arch = dt["archive_loc"].replace("model.tar.gz", "metrics.json")
+                with open(arch, "r") as inf:
+                    metrics =json.load(inf)
+                metrics["f1"] = np.mean([float(dt[v]["f1"]) for v in dt.keys() if "sentence" in v])
+                metrics["archive"] = dt["archive_loc"]
+                metrics['algo'] = dt['algorithm']
+                metrics['lm'] = np.mean([float(dt[v]["lm"]) for v in dt.keys() if "sentence" in v])
+                results[dt["algorithm"] + dt["archive_loc"]] = metrics
+                print(fn, metrics["f1"])
 
 with open("output/results.csv", "w") as of:
     first = True
