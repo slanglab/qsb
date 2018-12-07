6e079be0f86d207f267346b0337bb48253b99919
Abram Handler <ahandler@hobbes.cs.umass.edu>
2018-12-06 22:07:10 -0500

    ok

--- scripts/qsr.sh
+++ scripts/qsr.sh
@@ -3,10 +3,12 @@ source activate allennlp
 
 rm comp_experiments_f1/output/*
 
-
-
-python comp_experiments_f1/run_sentence.py -config comp_experiments_f1/experiments/prune_only_nn.json
-python comp_experiments_f1/run_sentence.py -config comp_experiments_f1/experiments/ilp.json
-python comp_experiments_f1/run_sentence.py -config comp_experiments_f1/experiments/nn_pred_then_prune.json
+# assess the convergence of ILP w/ validation set
+python comp_experiments_f1/run_f1_experiment.py -config comp_experiments_f1/experiments/ilp1.json
+python comp_experiments_f1/run_f1_experiment.py -config comp_experiments_f1/experiments/ilp2.json
+python comp_experiments_f1/run_f1_experiment.py -config comp_experiments_f1/experiments/ilp3.json
+python comp_experiments_f1/run_f1_experiment.py -config comp_experiments_f1/experiments/ilp4.json
+python comp_experiments_f1/run_f1_experiment.py -config comp_experiments_f1/experiments/ilp5.json
 
 python comp_experiments_f1/consolidator.py
+python comp_experiments_f1/run_f1_experiment.py -config comp_experiments_f1/experiments/nn-prune-greedy.json
