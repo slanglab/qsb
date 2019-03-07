113d6b33c5317a7785495a2da34ac909dbe19c30
Abram Handler <ahandler@hobbes.cs.umass.edu>
2019-03-07 17:34:55 -0500

    tme

--- bottom_up_clean/timer.py
+++ bottom_up_clean/timer.py
@@ -1,10 +1,20 @@
 
 import json
+import random
+import pickle
 from ilp2013.fillipova_altun import run_model
+from ilp2013.fillipova_altun_supporting_code import get_all_vocabs
 
 fn = "preproc/lstm_validation_sentences_3way.jsonl"
 
 S = []
+
+weights = "snapshots/8"
+
+with open(weights, "rb") as of:
+    weights = pickle.load(of)
+
+vocabs = get_all_vocabs()
 
 with open(fn, "r") as inf:
     for ln in inf:
@@ -13,7 +23,8 @@ with open(fn, "r") as inf:
 
 def test():
     """Stupid test function"""
-    L = [i for i in range(100)]
+    s = random.sample(S, k=1)[0]
+    run_model(s, r=s["r"], Q=s["q"], vocabs=vocabs, weights=weights) 
 
 if __name__ == '__main__':
     import timeit
