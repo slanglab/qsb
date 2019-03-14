3ad1b6d93916b8057cb6e8e3896516f4730c08c8
Abe Handler <akh2103@gmail.com>
2019-03-14 12:40:01 -0500

    cleanup

--- how_to_load.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import pdb
-
-from bottom_up.all import EasyAllenNLP
-
-m = EasyAllenNLP()
-paper_json = {"tokens": [{'word': "hi"}, {"word": "bye"}]}
-print(m.predict_proba(paper_json)) 
