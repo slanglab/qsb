cc41e9f94b86338623b9730f7ea4865721569556
Abram Handler <ahandler@hobbes.cs.umass.edu>
2019-03-25 18:20:04 -0400

    fix

--- scripts/bottom_up.sh
+++ scripts/bottom_up.sh
@@ -3,14 +3,15 @@
 
 echo "f1,slor mu, slor std,method" > bottom_up_clean/results.csv
 
-TRAIN=preproc/mini.training.paths
-VALIDATION=preproc/mini.validation.paths
+TRAIN=preproc/training.paths
+VALIDATION=preproc/validation.paths
 
-# get slor and f1 for random
-python bottom_up_clean/do_f1_and_slor.py -training_paths $TRAIN -validation_paths $VALIDATION -random
 
 # get slor and f1 for learned
 python bottom_up_clean/do_f1_and_slor.py -training_paths $TRAIN -validation_paths $VALIDATION
  
+# get slor and f1 for random
+python bottom_up_clean/do_f1_and_slor.py -training_paths $TRAIN -validation_paths $VALIDATION -random
+
 python bottom_up_clean/timer.py -path_to_set_to_evaluate $VALIDATION -N 10000
-py bottom_up_clean/ilp_wrapper.py -do_jsonl preproc/validation.jsonl
+python bottom_up_clean/ilp_wrapper.py -do_jsonl preproc/validation.jsonl
