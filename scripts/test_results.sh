34f59a9d916441e5d31325bc7caba71c38e8b094
Abram Handler <ahandler@hobbes.cs.umass.edu>
2019-04-04 19:15:07 -0400

    ok

---
+++ scripts/test_results.sh
@@ -0,0 +1,5 @@
+## get the results for the test set
+python bottom_up_clean/do_f1_and_slor.py -training_paths preproc/training.paths -skip_training -validation_paths preproc/test.paths
+python bottom_up_clean/ilp_wrapper.py -do_jsonl preproc/test.jsonl -ilp_snapshot 5
+py bottom_up_clean/ilp_wrapper.py -test_set_score -ilp_snapshot 5
+python code/significance_testing.py
