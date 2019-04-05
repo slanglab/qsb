d1be99b695526930d8a197cbf6d579de5023e661
Abram Handler <ahandler@hobbes.cs.umass.edu>
2019-04-05 05:53:06 -0400

    resu

--- scripts/test_results.sh
+++ scripts/test_results.sh
@@ -1,8 +1,21 @@
 ## get the results for the test set
 echo "" > bottom_up_clean/stat_sig.csv
+
+# get f1 and slor for additive + ablated
 python bottom_up_clean/do_f1_and_slor.py -training_paths preproc/training.paths -skip_training -validation_paths preproc/test.paths
+
+# get test set f1 and slor for vanilla ILP
 python bottom_up_clean/ilp_wrapper.py -do_jsonl preproc/test.jsonl -ilp_snapshot 5
-py bottom_up_clean/ilp_wrapper.py -test_set_score -ilp_snapshot 5
-python code/significance_testing.py
-python bottom_up_clean/timer.py -path_to_set_to_evaluate preproc/test.paths -N 5 -ilp_snapshot 5
+
+# get test set f1 for the vanilla ILP
+python bottom_up_clean/ilp_wrapper.py -test_set_score -ilp_snapshot 5
+
+# run timing experiments
+python bottom_up_clean/timer.py -path_to_set_to_evaluate preproc/test.paths -N 5 -ilp_snapshot 100000
+
+# do significance tests on time
 python code/significance_testing.py -file1 bottom_up_clean/timing/random.jsonl -file2 bottom_up_clean/timing/random.jsonl -metric time
+
+# do results on slor/f1
+python code/significance_testing.py -file1 bottom_up_clean/additive_results.jsonl -file2 bottom_up_clean/ilp_results.jsonl -metric slor
+python code/significance_testing.py -file bottom_up_clean/additive_results.jsonl -file2 
