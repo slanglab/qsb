0cc47db288b146bd94b4e63e05a5804cf72b7f68
Abram Handler <ahandler@hobbes.cs.umass.edu>
2019-04-15 11:18:25 -0400

    fix

---
+++ tmp_predictor_demo.py
@@ -0,0 +1,27 @@
+# note! 
+# export PYTHONPATH=.:allennlp2/allennlpallennlpasalibraryexample/
+from allennlp.predictors.predictor import Predictor
+from allennlp.models.archival import load_archive
+
+from allennlp2.allennlpallennlpasalibraryexample.my_library.models import *
+from allennlp2.allennlpallennlpasalibraryexample.my_library.dataset_readers import *
+from allennlp2.allennlpallennlpasalibraryexample.my_library.predictors import *
+
+import json
+archive_file = "models/457282793/model.tar.gz"
+archive = load_archive(archive_file=archive_file)
+predictor = Predictor.from_archive(archive, "paper-classifier") 
+
+
+fn = "allennlp2/allennlpallennlpasalibraryexample/tests/fixtures/s2_papers.jsonl"
+
+with open(fn, "r") as inf:
+    for _ in inf:
+        _ = json.loads(_)
+        print(_.keys())
+        sentence = _["sentence"] 
+        label = _["label"]
+        instance = predictor._dataset_reader.text_to_instance(sentence, label)
+
+
+
