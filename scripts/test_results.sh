14b98ebe1411708805f97162f0b6b9d5c601abfe
Abe Handler <akh2103@gmail.com>
2019-04-23 12:11:35 -0400

    just backup

--- scripts/test_results.sh
+++ scripts/test_results.sh
@@ -29,6 +29,12 @@ python bottom_up_clean/ilp_wrapper.py -do_jsonl preproc/test.jsonl -ilp_snapshot
 python bottom_up_clean/ilp_wrapper.py -test_set_score -ilp_snapshot 5
 
 # run timing experiments
+
+# get 3 seperate measurements 1 hour apart 
+python bottom_up_clean/timer.py -path_to_set_to_evaluate preproc/test.paths -N 100000 -ilp_snapshot 5
+sleep 1h
+python bottom_up_clean/timer.py -path_to_set_to_evaluate preproc/test.paths -N 100000 -ilp_snapshot 5
+sleep 1h
 python bottom_up_clean/timer.py -path_to_set_to_evaluate preproc/test.paths -N 100000 -ilp_snapshot 5
 
 # do significance tests
