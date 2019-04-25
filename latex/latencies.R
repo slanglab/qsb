20ce09c1677b683087cb225c1785283ccd822d96
Abe Handler <akh2103@gmail.com>
2019-04-24 22:00:06 -0400

    just backup

--- latex/latencies.R
+++ latex/latencies.R
@@ -1,15 +1,17 @@
 library(tidyverse)
 a <- read_csv("all_times.csv")
-a$time <- log(a$time)
+a$time <- log10(a$time)
 b <- a%>% filter(method=="additive" |  method == "ilp")
-b$`log seconds` <- b$time 
+b$`seconds (log scale)` <- b$time 
 
 b$method <- b$method %>% str_replace_all("additive", "vertex addition (lr)")
 
-ggplot(b, aes(x=`log seconds`, fill=method)) + geom_density(alpha = 0.2) + scale_fill_manual(values=c("#999999", "maroon")) + theme_minimal() + 
+formatBack <- function(x) 10^x 
+
+ggplot(b, aes(x=`seconds (log scale)`, fill=method)) + geom_density(alpha = 0.2) + scale_fill_manual(values=c("#999999", "maroon")) + theme_minimal() + 
 theme(axis.text.y = element_text(color="black", size=15), 
       axis.title=element_text(size=14,face="bold"),
       legend.title = element_text(size=14, face="bold"),
       legend.text = element_text(size=14, face="bold"),
-      axis.text.x = element_text(color="black", size=15))
+      axis.text.x = element_text(color="black", size=15)) + scale_x_continuous(labels=function(x) 10^x ) +
 ggsave("times.pdf", width=7, height=1.5)
