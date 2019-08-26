ed66da227fbc434b5e261a70b69853e044c582ed
Abe Handler <akh2103@gmail.com>
2019-08-26 09:28:26 -0400

    just backup

--- scripts/rollup_times.R
+++ scripts/rollup_times.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 a <- read_csv("bottom_up_clean/all_times.csv")
+a$time = log(a$time)
 b <- a %>% group_by(method) %>% summarise(mean(time))
-c <- a %>% group_by(method) %>% summarise(sd(time))
 
+b$`mean(time)` = exp(b$`mean(time)`)
 
 # A tibble: 5 x 2
 #  method      `mean(time)`
@@ -13,6 +14,5 @@ c <- a %>% group_by(method) %>% summarise(sd(time))
 #4 ilp             0.0567
 #5 random          0.000736
 
-d <- inner_join(b,c)
 
-write_csv(d, "bottom_up_clean/all_times_rollup.csv")
+write_csv(b, "bottom_up_clean/all_times_rollup.csv")
