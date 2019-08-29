ec0ec8215bb5c217c8f5b219eb5c920f480ef326
Abe Handler <akh2103@gmail.com>
2019-08-29 16:08:32 -0400

    just backup

--- scripts/unzip_paths.sh
+++ scripts/unzip_paths.sh
@@ -4,3 +4,4 @@ unzip preproc/training_paths2.zip -d preproc
 unzip preproc/test.paths.zip  preproc/test.paths
 
 cat preproc/xa* > preproc/training.paths
+unzip preproc/validation.paths.zip preproc/validation.paths
